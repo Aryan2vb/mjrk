5df49fa800245ce60e01ca88279fd90878876b6a
Aryan2vb <aryansoni2105@gmail.com>
2024-11-11 12:19:58 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use('/api', customerRoutes);
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to Customer Management API!');
 });
-app.get('hello',async (req,res)=>{
+app.get('/hello', (req,res)=>{
     res.status(200).send('Sab Changa Si');
 })
 
